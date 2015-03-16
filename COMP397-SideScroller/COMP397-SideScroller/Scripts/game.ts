56fc2fc5e6ca1382d4832b209eb5eeda057c402c
Tim Chiu <Tim.Chiu@live.ca>
2015-03-16 18:17:40 -0400

    Side Scroller Demo

---
+++ COMP397-SideScroller/COMP397-SideScroller/Scripts/game.ts
@@ -0,0 +1,104 @@
+﻿/// <reference path="typings/createjs-lib/createjs-lib.d.ts" />
+/// <reference path="typings/easeljs/easeljs.d.ts" />
+/// <reference path="typings/tweenjs/tweenjs.d.ts" />
+/// <reference path="typings/soundjs/soundjs.d.ts" />
+/// <reference path="typings/preloadjs/preloadjs.d.ts" />
+
+/// <reference path="typings/stats/stats.d.ts" />
+
+
+/// <reference path="objects/gameobject.ts" />
+/// <reference path="objects/scoreboard.ts" />
+/// <reference path="objects/plane.ts" />
+/// <reference path="objects/island.ts" />
+/// <reference path="objects/cloud.ts" />
+/// <reference path="objects/ocean.ts" />
+
+
+/// <reference path="states/gameplay.ts" />
+
+
+
+
+
+
+// Global game Variables
+var canvas;
+var stage: createjs.Stage;
+var assetLoader: createjs.LoadQueue;
+var stats: Stats = new Stats();
+
+var gamePlay: states.GamePlay;
+
+var manifest = [
+    { id: "cloud", src: "assets/images/cloud.png" },
+    { id: "island", src: "assets/images/island.png" },
+    { id: "ocean", src: "assets/images/ocean.gif" },
+    { id: "plane", src: "assets/images/plane.png" },
+    { id: "engine", src: "assets/audio/engine.ogg" },
+    { id: "yay", src: "assets/audio/yay.ogg" },
+    { id: "thunder", src: "assets/audio/thunder.ogg" }
+];
+
+
+
+
+
+
+
+
+function Preload() {
+    assetLoader = new createjs.LoadQueue(); // create a new preloader
+    assetLoader.installPlugin(createjs.Sound); // need plugin for sounds
+    assetLoader.on("complete", init, this); // when assets finished preloading - then init function
+
+    assetLoader.loadManifest(manifest);
+
+}
+
+
+
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    stage.enableMouseOver(20); // Enable mouse events
+    createjs.Ticker.setFPS(60); // 60 frames per second
+    createjs.Ticker.addEventListener("tick", gameLoop);
+    setupStats();
+
+    main();
+}
+
+function setupStats() {
+    stats.setMode(0); 
+
+    // align top-left
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '650px';
+    stats.domElement.style.top = '440px';
+
+    document.body.appendChild(stats.domElement);
+}
+
+
+function gameLoop() {
+    stats.begin();
+
+    gamePlay.update();
+
+    stats.end();
+}
+
+
+function main() {
+
+    // Instantiate new Game State
+    gamePlay = new states.GamePlay();
+
+
+}
+
+
+
+
+
